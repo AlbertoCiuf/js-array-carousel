98fa8d6a23132b6ea9db0a67a830883a7e95fe91
AlbertoCiuf <alberton.ciufici@gmail.com>
2021-10-27 16:32:09 +0200

    carousel funzionante con elementi scritti in html

--- js/script.js
+++ js/script.js
@@ -21,3 +21,31 @@ const text = [
   'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
   'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
 ];
+
+
+const item = document.getElementsByClassName('item');
+//console.log(item);
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+let contatore = 0;
+// console.log(item[contatore]);
+
+prev.addEventListener('click', function(){
+  item[contatore].classList.remove('active');
+  contatore--;
+  if (contatore < 0) {
+    contatore = item.length-1;
+  }
+  item[contatore].classList.add('active');
+
+});
+
+next.addEventListener('click', function(){
+  item[contatore].classList.remove('active');
+  contatore++;
+  if (contatore > item.length - 1) {
+    contatore = 0;
+  }
+  item[contatore].classList.add('active');
+});
